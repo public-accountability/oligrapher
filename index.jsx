e08188abb1ac1b1ee361dc4831ac0560b52f4351
Mudit Ameta <zeusdeux@gmail.com>
2015-05-13 22:24:02 -0400

    Fixed jshint issue and fixed some boilerplate code

--- index.jsx
+++ index.jsx
@@ -1,9 +1,14 @@
-'use strict'
-
 var React = require('react');
 var SearchContainer = require('./SearchContainer');
 
-React.render(
-  <SearchContainer />,
-  document.getElementById('content')
-);
+var MainClass = React.createClass({
+  render: function () {
+    'use strict';
+    return (
+      <SearchContainer />,
+      document.getElementById('content')
+    );
+  }
+});
+
+module.exports = MainClass;
