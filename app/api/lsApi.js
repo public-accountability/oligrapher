5c3a354048060cc894eab21c14622abcf7277ee8
aguestuser <guest.austin@gmail.com>
2015-05-22 23:20:55 -0400

    Add specs for lsApi#getAdjacentEntities

--- app/api/lsApi.js
+++ app/api/lsApi.js
@@ -10,23 +10,22 @@ var lsApi = {};
 
 //searchEntities(String, Function) -> Unit
 lsApi.searchEntities = (query, handleResponse) => {
-  var unwrapThis = (res) => unwrap(res).Entities.Entity || [];
-  return new Promise((resolve, reject) => {
+  var unwrapThis = res => unwrap(res).Entities.Entity || [];
+  return new Promise((resolve, reject) =>
     request
       .get(baseUrl + 'entities.json')
       .query({q: query, _key: apiKey})
-      .end((err, res) => err ? reject(err) : resolve(unwrapThis(res)));
-  });
+      .end((err, res) => err ? reject(err) : resolve(unwrapThis(res))));
 };
 
 //getAdjacentEntities(String, Function) -> Unit
 lsApi.getAdjacentEntities = (id, handleResponse) => {
-  request
-    .get(baseUrl + 'entity/' + id + 'related/degree2.json')
-    .query({ cat1_ids: 1, cat2_ids: 1, order1: 2, order2: 1, _key: apiKey })
-    .end((err, res) => {
-      handleResponse(err, unwrap(res).Degree2Entities.Entity); });
+  var unwrapThis = res =>  unwrap(res).Degree2Entities.Entity || [];
+  return new Promise((resolve, reject) =>
+    request
+      .get(baseUrl + 'entity/' + id + '/related/degree2.json')
+      .query({ cat1_ids: 1, cat2_ids: 1, order1: 2, order2: 1, _key: apiKey })
+      .end((err, res) => err ? reject(err) : resolve(unwrapThis(res))));
 };
-
 
 module.exports = lsApi;
--- test/lsApi_specs.js
+++ test/lsApi_specs.js
@@ -11,14 +11,24 @@ describe('LittleSis API Client', () => {
   describe('searching Entities', () => {
 
     it("returns an array of entities when the query mathces an entity in LittleSis ", () =>
-
        Promise.resolve(lsApi.searchEntities('walmart'))
-         .should.eventually.eql(sd.expectedSearchEntitiesResult));
+         .should.eventually.eql(sd.searchEntitiesResult));
 
     it("returns an empty array when the query doesn't match anything", () =>
-
        Promise.resolve(lsApi.searchEntities(',,'))
          .should.eventually.eql([]));
+
+  });
+
+  describe('getting Adjacent Entities', () => {
+
+    it('returns an array of entities when the entity has adjacencies', () =>
+       Promise.resolve(lsApi.getAdjacentEntities('1'))
+         .should.eventually.eql(sd.getAdjacentEntitiesResult));
+
+    it("throws an error when the queried entity doesn't exist", () =>
+       Promise.resolve(lsApi.getAdjacentEntities('0'))
+         .should.be.rejectedWith('Not Found'));
 
   });
 
