f6efb63d8cdac1aa91040ec0dd5c85ce20c96552
Matthew Skomarovsky <matthew@verypolite.com>
2015-08-19 14:19:07 -0400

    add opengraph title and image tags to deck page

--- app/components/GraphContainer.jsx
+++ app/components/GraphContainer.jsx
@@ -1,6 +1,7 @@
 const Marty = require('marty');
 const BaseComponent = require('./BaseComponent');
 const Graph = require('./Graph');
+const DocMeta = require('react-doc-meta');
 
 class GraphContainer extends BaseComponent {
   constructor(options){
@@ -9,6 +10,11 @@ class GraphContainer extends BaseComponent {
   }
 
   render(){
+    const tags = [
+      { property: "og:title", content: this.props.deck.title + " - LittleSis" },
+      { property: "og:image", content: "http://s3.amazonaws.com/pai-littlesis/images/maps/" + this.props.deck.id + ".png" }
+    ];
+
     const zoomButtons = this.props.graph.isNull() ? null : (
       <div id="zoomButtons">
         <button id="zoom-in" className="zoomButton" onClick={this._handleZoomIn}>+</button>
@@ -18,6 +24,7 @@ class GraphContainer extends BaseComponent {
 
     return (
       <div className="graphContainer">
+        <DocMeta tags={tags} />
         {zoomButtons}
         <Graph graph={this.props.graph} />
       </div>
@@ -38,6 +45,9 @@ module.exports = Marty.createContainer(GraphContainer, {
   fetch: {
     graph() {
       return this.app.graphStore.getGraph(this.app.deckStore.getCurrentGraphId());
+    },
+    deck() {
+      return this.app.deckStore.getCurrentDeck();
     }
   }
 });
