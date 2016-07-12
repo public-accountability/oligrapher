882a5c3f929d05dc33aac28421c2fdefe0ff7b97
Jess <jessicapeter92@gmail.com>
2016-07-12 11:57:45 -0400

    annotation nav buttons aren't shown unless in editor mode or if more than one annotation is available

--- app/components/GraphNavButtons.jsx
+++ app/components/GraphNavButtons.jsx
@@ -3,16 +3,22 @@ import React, { Component, PropTypes } from 'react';
 export default class GraphNavButtons extends Component {
 
   render() {
+    var shouldShowNav = this.props.isEditor || this.props.annotations.length > 1;
+
     return (
       <div id="oligrapherNavButtons">
-        <button 
-          className="btn btn-lg btn-default" 
-          onClick={this.props.prevClick} 
-          disabled={!this.props.canClickPrev}>Prev</button>
-        <button 
-          className="clickplz btn btn-lg btn-default" 
-          onClick={this.props.nextClick} 
-          disabled={!this.props.canClickNext}>Next</button>
+      { shouldShowNav && 
+          <div id="oligrapherAnnotationsNavHolder">
+            <button 
+              className="btn btn-lg btn-default" 
+              onClick={this.props.prevClick} 
+              disabled={!this.props.canClickPrev}>Prev</button>
+            <button 
+              className="clickplz btn btn-lg btn-default" 
+              onClick={this.props.nextClick} 
+              disabled={!this.props.canClickNext}>Next</button>
+          </div>
+      }
         <div style={{ float: "right" }}>
           <button
             id="oligrapherHideAnnotationsButton"
