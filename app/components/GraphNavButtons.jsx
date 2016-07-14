5e4271b302ee00fe98174a2451adf4ce2adfd292
Jess <jessicapeter92@gmail.com>
2016-07-14 16:59:35 -0400

    merged conflict caused by title attribute

--- app/components/GraphNavButtons.jsx
+++ app/components/GraphNavButtons.jsx
@@ -3,18 +3,24 @@ import React, { Component, PropTypes } from 'react';
 export default class GraphNavButtons extends Component {
 
   render() {
+    var shouldShowNav = this.props.isEditor || this.props.annotations.length > 1;
+
     return (
       <div id="oligrapherNavButtons">
-        <button 
-          title="previous annotation" 
-          className="btn btn-lg btn-default" 
-          onClick={this.props.prevClick} 
-          disabled={!this.props.canClickPrev}>Prev</button>
-        <button
-          title="next annotation" 
-          className="clickplz btn btn-lg btn-default" 
-          onClick={this.props.nextClick} 
-          disabled={!this.props.canClickNext}>Next</button>
+      { shouldShowNav && 
+          <div id="oligrapherAnnotationsNavHolder">
+            <button 
+              title="previous annotation"
+              className="btn btn-lg btn-default" 
+              onClick={this.props.prevClick} 
+              disabled={!this.props.canClickPrev}>Prev</button>
+            <button 
+              title="next annotation"
+              className="clickplz btn btn-lg btn-default" 
+              onClick={this.props.nextClick} 
+              disabled={!this.props.canClickNext}>Next</button>
+          </div>
+      }
         <div style={{ float: "right" }}>
           <button
             title="show/hide annotations"
