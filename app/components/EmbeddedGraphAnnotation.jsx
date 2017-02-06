c58857b24f98c96d6e3ac33897fd53acda168317
ziggy <ziggy@elephant-bird.net>
2017-02-06 17:13:01 -0500

    Add server to test embedded oligrapher in article

--- app/components/EmbeddedGraphAnnotation.jsx
+++ app/components/EmbeddedGraphAnnotation.jsx
@@ -1,19 +1,19 @@
 import React, { Component, PropTypes } from 'react';
-import BaseComponent from "./BaseComponent";
 
-export default class EmbeddedGraphAnnotation extends BaseComponent {
-  constructor(props) {
-    super(props);
-  }
+export default class EmbeddedGraphAnnotation extends Component {
 
   render() {
     let { header, text } = this.props.annotation;
     
     return (
       <div id="oligrapherEmbeddedGraphAnnotation">
-	  <h3>{header}</h3>
-	  <div id="oligrapherEmbeddedGraphAnnotationText"
-	       dangerouslySetInnerHTML={{ __html: this.props.annotation.text }}>
+	  <div class="row">
+	      <h3>{header}</h3>
+	      <div class="col-sm-8">
+		  <div id="oligrapherEmbeddedGraphAnnotationText"
+		       dangerouslySetInnerHTML={{ __html: this.props.annotation.text }}>
+		  </div>
+	      </div>
 	  </div>
       </div>
       
