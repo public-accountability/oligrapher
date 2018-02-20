d12f9257aea6f71e904347aa8d83bf293e9069ac
ziggy <ziggy@elephant-bird.net>
2018-02-20 16:15:06 -0500

    update all dependencies to latest version (except react)

--- app/main.jsx
+++ app/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import createLogger from 'redux-logger';
+import { createLogger } from 'redux-logger'
 import thunk from 'redux-thunk';
 import Root from './components/Root';
 import reducers from './reducers';
