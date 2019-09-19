6aa13623c40975d8b4f83ece3741a8ea0ab67791
ziggy <ziggy@littlesis.org>
2019-09-19 15:30:23 -0400

    add findNodes

--- app/datasources/littlesis.js
+++ app/datasources/littlesis.js
@@ -1,7 +1,23 @@
 import { client } from "@public-accountability/littlesis-api"
 
-export const littlesisDataSource = {
-  name: "LittleSis"
+// const api = client('https://littlesis.org');
+const api = client('https://littlesis.org');
+
+
+export function findNodes(text, callback) {
+  let params = {
+    q: text,
+    num: 12,
+    desc: true,
+    with_ids: true    
+  }  
+
+  return api.get('/maps/find_nodes', params).then(callback);
 }
 
-export default littlesisDataSource;
+
+export default {
+  name: "LittleSis",
+  findNodes: findNodes
+}
+
