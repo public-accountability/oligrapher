f4e1491bea1d53775e323b33730e70dba2e6ab71
ziggy <ziggy@littlesis.org>
2019-10-09 17:35:11 -0400

    more?.new?.javascript

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,19 @@
-module.exports = function (api) {
+module.exports = function(api) {
   // These are run in reserve order. @babel/preset-react is run first.
   const presets = [
     [ "@babel/preset-env",   { useBuiltIns: "usage", corejs: 3 } ],
     [ "@babel/preset-react", {} ]
-  ];
+  ]
 
-  const plugins = ["@babel/plugin-proposal-class-properties"];
+  const plugins = [
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-optional-chaining"
+  ]
 
-  api.cache.never();
+  api.cache.never()
 
   return {
     presets,
     plugins
-  };
-};
+  }
+}
