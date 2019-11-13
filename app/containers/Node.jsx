60b2eb37a7d32fc00b35e2768221022698cf4fe0
ziggy <ziggy@littlesis.org>
2019-11-13 18:01:12 -0500

    Cleanup Node.jsx

--- app/containers/Node.jsx
+++ app/containers/Node.jsx
@@ -1,20 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
-import Draggable from 'react-draggable'
 import DraggableNode from './../components/graph/DraggableNode'
 import NodeCircle from './../components/graph/NodeCircle'
 
-import { xy } from '../util/helpers'
-
-import merge from 'lodash/merge'
 import pick from 'lodash/pick'
 
 const DEFAULT_COLOR = "#ccc"
 
 export function Node(props) {
-
   return <DraggableNode onStop={props.onStop} actualZoom={props.actualZoom} >
            <g id={"node-" + props.id} className="oligrapher-node">
              <NodeCircle {...pick(props, ['x', 'y', 'scale', 'color'])} />
@@ -42,19 +37,17 @@ Node.defaultProps = {
 
 const mapStateToProps = (state, ownProps) => {
   const id = ownProps.id.toString()
-  const node = state.graph.nodes[id]
+  const nodeDisplay = state.graph.nodes[id].display
   const actualZoom = state.graph.actualZoom
 
-  return { ...node.display, id, actualZoom }
+  return { ...nodeDisplay, id, actualZoom }
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   const id = ownProps.id.toString()
 
   return {
-    onStop: (deltas) => dispatch({ type: 'MOVE_NODE',
-                                   id: id,
-                                   deltas: deltas })
+    onStop: (deltas) => dispatch({ type: 'MOVE_NODE', id: id, deltas: deltas })
   }
 }
 
