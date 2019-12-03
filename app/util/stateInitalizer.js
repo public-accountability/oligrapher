6fc67a3abe6fb6ed9a4d2d72360f829d69f9652c
ziggy <ziggy@littlesis.org>
2019-12-03 15:54:28 -0500

    Flatten Nodes and Edges

--- app/util/stateInitalizer.js
+++ app/util/stateInitalizer.js
@@ -1,29 +1,38 @@
 import { getElementById } from './helpers'
 import defaultState from './defaultState'
 import Graph from '../graph/graph'
-import Curve from '../graph/curve'
+// import Curve from '../graph/curve'
 
 import merge from 'lodash/merge'
-import pick from 'lodash/pick'
+import omit from 'lodash/omit'
+// import pick from 'lodash/pick'
 
 
-// Oligrapher 2 used to store these properties: { cx, cy, x1, y1, x2, y2, s1, s2 } on the `display` elements.
-// It was replaced with the `curve` and `isReverse` field
-function transformEdge(legacyEdge) {
-  // return early if already in new format
-  if (legacyEdge.display.curve) {
-    return legacyEdge
-  }
-  const geometry = Curve.util.calculateGeometry(legacyEdge.display)
-  const curve = Curve.from.geometry(geometry)
-  const isReverse = geometry.is_reverse
-
-  return merge(
-    pick(legacyEdge, ['id', 'node1_id', 'node2_id']),
-    { display: pick(legacyEdge.display, ['status', 'label', 'scale', 'arrow', 'dash', 'url']) },
-    { display: { curve, isReverse } }
-  )
+/*
+  Edge and Node used to contain a `display` element.
+  This has been simplified to be a flat object without any nested components.
+*/
+export function flatten(obj) {
+  return merge(omit(obj, 'display'), obj.display)
 }
+
+// // Oligrapher 2 used to store these properties: { cx, cy, x1, y1, x2, y2, s1, s2 } on the `display` elements.
+// // It was replaced with the `curve` and `isReverse` field
+// function transformEdge(legacyEdge) {
+//   // return early if already in new format
+//   if (legacyEdge.display.curve) {
+//     return legacyEdge
+//   }
+//   const geometry = Curve.util.calculateGeometry(legacyEdge.display)
+//   const curve = Curve.from.geometry(geometry)
+//   const isReverse = geometry.is_reverse
+
+//   return merge(
+//     pick(legacyEdge, ['id', 'node1_id', 'node2_id']),
+//     { display: pick(legacyEdge.display, ['status', 'label', 'scale', 'arrow', 'dash', 'url']) },
+//     { display: { curve, isReverse } }
+//   )
+// }
 
 /*
   Transforms oligrapher's serialized state (plain json) into correct format
@@ -34,10 +43,14 @@ export default function stateInitalizer(serializedState) {
   let state = merge({}, defaultState, serializedState)
   state.graph = Graph.new(serializedState.graph)
 
-  /// Transform legacy Edges
   Object.keys(state.graph.edges).forEach(edgeId => {
     let edge = state.graph.edges[edgeId]
-    state.graph.edges[edge.id] = transformEdge(edge)
+    state.graph.edges[edge.id] = flatten(edge)
+  })
+
+  Object.keys(state.graph.nodes).forEach(nodeId => {
+    let node = state.graph.nodes[nodeId]
+    state.graph.nodes[node.id] = flatten(node)
   })
 
   state.settings.rootElement = getElementById(state.settings.domId)
--- test/util/stateInitalizer.test.js
+++ test/util/stateInitalizer.test.js
@@ -1,9 +1,9 @@
 import values from 'lodash/values'
 import * as helpers from '../../app/util/helpers'
 import Node from '../../app/graph/node'
-import stateInitalizer from '../../app/util/stateInitalizer'
+import stateInitalizer, { flatten } from '../../app/util/stateInitalizer'
 
-describe('stateInitalizer', function() {
+describe.only('stateInitalizer', function() {
   const fakeElement = { height: 100, width: 100 }
 
   beforeEach(function() {
@@ -28,5 +28,10 @@ describe('stateInitalizer', function() {
     let initialState = stateInitalizer(serializedState)
     expect(values(initialState.graph.nodes)).to.have.lengthOf(1)
     expect(initialState.graph.edges).to.eql({})
+  })
+
+  specify('flatten()', function() {
+    let legacyEdge = { id: 'abc', node1_id: 1, node2_id: 2, display: { arrow: false, status: 'normal' }}
+    expect(flatten(legacyEdge)).to.eql({ id: 'abc', node1_id: 1, node2_id: 2, arrow: false, status: 'normal' })
   })
 })
