8316036d23ae0a7470b59863f1f4b741ec90cca2
ziggy <ziggy@littlesis.org>
2020-01-02 13:36:19 -0500

    add class "edge-tool-open" to <Nodes>

--- app/containers/Nodes.jsx
+++ app/containers/Nodes.jsx
@@ -3,21 +3,27 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import isEqual from 'lodash/isEqual'
 
+import { classNames } from '../util/helpers'
 import Node from './Node'
 
 export function Nodes(props) {
-  return <g className="nodes">
+  return <g className={props.className}>
            { props.nodeIds.map( id => <Node key={id} id={id} /> )  }
          </g>
 }
 
 Nodes.propTypes = {
-  nodeIds: PropTypes.arrayOf(PropTypes.string)
+  nodeIds: PropTypes.arrayOf(PropTypes.string),
+  className: PropTypes.string.isRequired
 }
 
 const mapStateToProps = function(state) {
+  const edgeToolOpen = state.display.editor.tool === 'edge'
+  const className = classNames('nodes', edgeToolOpen ? 'edge-tool-open' : null)
+
   return {
-    nodeIds: Object.keys(state.graph.nodes)
+    nodeIds: Object.keys(state.graph.nodes),
+    className: className
   }
 }
 
