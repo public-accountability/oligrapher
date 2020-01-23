26d096db604dde32dff2d7b752d3c2cc46299bba
ziggy <ziggy@littlesis.org>
2020-01-23 12:31:29 -0500

    test for Curve.calculateGeometry

--- test/graph/curve.test.js
+++ test/graph/curve.test.js
@@ -1,6 +1,8 @@
 import Curve from '../../app/graph/curve'
 import Node from '../../app/graph/node'
 
+import { xy } from '../../app/util/helpers'
+
 describe('Curve', function() {
   describe('helper functions', function() {
     let n1, n2
@@ -25,6 +27,31 @@ describe('Curve', function() {
       expect(Curve.util.parseCurveString(curveString))
         .to.eql([ [68.02, -68.47], [105, -93.8], [114.8, -143.595] ])
 
+    })
+  })
+
+  describe('calculateGeometry', function() {
+    let edge = { cx: null, cy: null, x1: 0, y1: 0, x2: 10, y2: 0, s1: 1, s2: 1 }
+
+    it('calculates midpoint', function(){
+      expect(xy(Curve.calculateGeometry(edge))).to.eql({x: 5, y: 0})
+    })
+
+    it('sets cx, cy', function() {
+      const geom = Curve.calculateGeometry(edge)
+      expect(geom.cx).to.be.a('number')
+      expect(geom.cy).to.be.a('number')
+    })
+
+    it('sets xa,ya, xb, yb', function() {
+      const geom = Curve.calculateGeometry(edge)
+      const keys = ['xa', 'ya', 'xb', 'yb']
+      keys.forEach( key => expect(geom[key], `${key} is not a number`).to.be.a('number'))
+    })
+
+    it("sets is_reverse", function(){
+      expect(Curve.calculateGeometry(edge).is_reverse).to.be.false
+      expect(Curve.calculateGeometry({...edge, x2: -1}).is_reverse).to.be.true
     })
   })
 })
