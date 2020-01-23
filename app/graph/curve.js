1667aced34354c047c7c022168420fb541a5e0cc
ziggy <ziggy@littlesis.org>
2020-01-23 12:06:11 -0500

    cleanup curve.js and tests

--- app/graph/curve.js
+++ app/graph/curve.js
@@ -25,132 +25,17 @@ const curveStrength = 0.5
 const circleRadius = 25
 const circleSpacing = 4
 
-// (Node, Node) --> Node
-function leftNode(a, b) {
-  if (a.x <= b.x) {
-    return a
-  } else {
-    return b
-  }
-}
-
-function rightNode(a, b) {
-  if (leftNode(a, b).id === a.id) {
-    return b
-  } else {
-    return a
-  }
-}
-
-export function midpoint(start, end) {
+function midpoint(start, end) {
   let x = (start.x + end.x) / 2
   let y = (start.y + end.y) / 2
   return {x, y}
 }
 
-// Node, Node, Number ---> { x: number, y: number }
-// export function defaultOffset(leftNode, rightNode, curveStrength = 0.5) {
-//   const midP = midpoint(leftNode, rightNode)
-//   const x = -(leftNode.display.y - midP.y)
-//   const y = leftNode.display.x  - midP.x
-
-//   return mapValues({ x, y }, n => n * curveStrength)
-// }
-
-// export function defaultControlPoint(node1, node2) {
-//   const nodeL = leftNode(node1, node2)
-//   const nodeR = rightNode(node1, node2)
-//   const midP = midpoint(nodeL, nodeR)
-//   const offset = defaultOffset(nodeL, nodeR)
-
-//   return { x: midP.x + offset.x,
-//            y: midP.y + offset.y }
-
-// }
-
-// export function curveString(p1, p2, p3) {
-//   return `M ${p1.x} ${p1.y} Q ${p2.x} ${p2.y} ${p3.x} ${p3.y}`
-// }
-
-
 // string --> [ [int, int] ]
-export function parseCurveString(str) {
+function parseCurveString(str) {
   return chunk(str.replace(/(M|Q)/g, '').trim().split(/[ ]+/).map(toNumber), 2)
 }
 
-/*
-  Creates a curve string used by the <path> element to draw edges
-  The curve is a Quadratic Bezier curve.
-  It takes 3 points (p1, p2, p3) to draw the curve. We also refer to p2 as the "control" point.
-
-  See the documentation on Q paths here: https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Paths
-
-  node1 and node2 are required. If the control point is not provided a default value will be calculated.
-*/
-// export function calculateCurve(node1, node2) {
-//   const p1 = xy(leftNode(node1, node2).display)
-//   const p2 = defaultControlPoint(node1, node2)
-//   const p3 = xy(rightNode(node1, node2).display)
-//   return curveString(p1, p2, p3)
-// }
-
-
-/*
-  input: curveString, side (START or END), deleats
-
-*/
-// export function moveCurvePointOld(curve, side, deltas) {
-//   const [p1, p2, p3] = parseCurveString(curve)
-
-//   if (side === 'START') {
-//     return curveString(
-//       translatePoint(xyFromArray(p1), deltas),
-//       xyFromArray(p2),
-//       xyFromArray(p3)
-//     )
-//   } else if (side === 'END') {
-//     return curveString(
-//       xyFromArray(p1),
-//       xyFromArray(p2),
-//       translatePoint(xyFromArray(p3), deltas)
-//     )
-//   } else {
-//     throw new Error("Invalid side")
-//   }
-
-// }
-
-// function angleBetweenPoints(a, b) {
-//   return Math.atan2(b.y - a.y, b.x - a.x)
-// }
-
-// function rotatePoint(point, angle) {
-//   const cos = Math.cos(angle)
-//   const sin = Math.sin(angle)
-
-//   return { x: point.x * cos - point.y * sin,
-//            y: point.x * sin + point.y * cos }
-// }
-
-// export function moveCurvePoint(curveString, side, deltas) {
-//   const curve = parseCurveString(curveString).map(xyFromArray)
-//   const futureCurve = clone(curve)
-
-//   if (side === 'START') {
-//     futureCurve[0] = translatePoint(curve[0], deltas)
-//   } else {
-//     futureCurve[2] = translatePoint(curve[2], deltas)
-//   } It's fine so far. They have lots of extra space in their office so I'm basically just working away like a I always do...
-
-
-
-//   const deltaAngle = angleBetweenPoints(futureCurve[0], futureCurve[2]) - angleBetweenPoints(curve[0], curve[2])
-
-//   futureCurve[1] = rotatePoint(curve[1], deltaAngle)
-
-//   return curveString(...futureCurve)
-
-// }
 export function calculateGeometry(edge) {
   let { cx, cy, x1, y1, x2, y2, s1, s2 } = edge
   let r1 = s1 * circleRadius
@@ -213,7 +98,6 @@ export function calculateGeometry(edge) {
   return { x, y, cx, cy, xa, ya, xb, yb, is_reverse }
 }
 
-
 // `M ${xa}, ${ya} Q ${x + cx}, ${y + cy}, ${xb}, ${yb}`,
 export function curveFromGeometry(geometry) {
   const start = [geometry.xa, geometry.ya]
@@ -222,27 +106,14 @@ export function curveFromGeometry(geometry) {
   return "M " + start.join(' ') + " Q " + control.concat(end).join(' ')
 }
 
-// export function curveFromLegacyEdge(edgeDisplay) {
-//   return curveFromGeometry(calculateGeometry(edgeDisplay))
-// }
-// util: {
-  //   midpoint: midpoint,
-  //   // defaultControlPoint: defaultControlPoint,
-  //   // defaultOffset: defaultOffset,
-  //   parseCurveString: parseCurveString,
-  //   calculateGeometry: calculateGeometry
-  // },
-  // from: {
-  //   geometry: curveFromGeometry,
-  //   // legacyEdge: curveFromLegacyEdge,
-  //   // nodes: calculateCurve
-  // }
-
 export default {
   calculateGeometry: calculateGeometry,
-  curveFromGeometry: curveFromGeometry,
   from: {
     geometry: curveFromGeometry,
     edge: edge => curveFromGeometry(calculateGeometry(edge)),
+  },
+  util: {
+    midpoint: midpoint,
+    parseCurveString: parseCurveString
   }
 }
--- test/graph/curve.test.js
+++ test/graph/curve.test.js
@@ -1,29 +1,30 @@
 import Curve from '../../app/graph/curve'
 import Node from '../../app/graph/node'
 
-xdescribe('Curve', function() {
-  let n1, n2
+describe('Curve', function() {
+  describe('helper functions', function() {
+    let n1, n2
 
-  beforeEach(function() {
-    n1 = Node.new({x: 0, y: 0 })
-    n2 = Node.new({x: 10, y: 10 })
-  })
+    beforeEach(function() {
+      n1 = Node.new({x: 0, y: 0 })
+      n2 = Node.new({x: 10, y: 10 })
+    })
 
-  specify('midpoint()', function() {
-    expect(Curve.util.midpoint(n1, n2)).to.eql({x: 5, y: 5})
-  })
+    specify('midpoint()', function() {
+      expect(Curve.util.midpoint(n1, n2)).to.eql({x: 5, y: 5})
+    })
 
-  specify('parseCurveString with space', function() {
-    let curveString = "M 0 0 Q -200 -300 2 3"
-    expect(Curve.util.parseCurveString(curveString))
-      .to.eql([[0,0], [-200, -300], [2,3]])
+    specify('parseCurveString with space', function() {
+      let curveString = "M 0 0 Q -200 -300 2 3"
+      expect(Curve.util.parseCurveString(curveString))
+        .to.eql([[0,0], [-200, -300], [2,3]])
+    })
 
-  })
+    specify('parseCurveString w/o space', function() {
+      let curveString = "M 68.02 -68.47 Q 105 -93.8 114.8 -143.595"
+      expect(Curve.util.parseCurveString(curveString))
+        .to.eql([ [68.02, -68.47], [105, -93.8], [114.8, -143.595] ])
 
-  specify('parseCurveString w/o space', function() {
-    let curveString = "M 68.02 -68.47 Q 105 -93.8 114.8 -143.595"
-    expect(Curve.util.parseCurveString(curveString))
-      .to.eql([ [68.02, -68.47], [105, -93.8], [114.8, -143.595] ])
-
+    })
   })
 })
