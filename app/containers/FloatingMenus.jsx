441471114c96b3843e6017becd7daeb8bfc503d9
ziggy <ziggy@littlesis.org>
2020-02-19 09:47:00 -0500

    add stub Add Connections Menu

--- app/containers/FloatingMenus.jsx
+++ app/containers/FloatingMenus.jsx
@@ -5,13 +5,16 @@ import { useSelector } from 'react-redux'
 import EditNodeMenu from './EditNodeMenu'
 import EditEdgeMenu from './EditEdgeMenu'
 import EditCaptionMenu from './EditCaptionMenu'
+import AddConnectionsMenu from './AddConnectionsMenu'
 import Settings from './Settings'
 
 export default function FloatingMenus() {
   const openMenu = useSelector(state => state.display.floatingMenu)
+
   return <>
            <div style={{width: 0, overflow: "hidden"}} id="caption-text-input"></div>
            { openMenu === 'node' && <EditNodeMenu /> }
+           { openMenu === 'connections' && <AddConnectionsMenu /> }
            { openMenu === 'edge' && <EditEdgeMenu /> }
            { openMenu === 'caption' && <EditCaptionMenu /> }
            { openMenu === 'settings' && <Settings /> }
