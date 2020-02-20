3cfe38e83dd287cf6cc11ccc80ae9a1c30f7b747
ziggy <ziggy@littlesis.org>
2020-02-20 18:19:30 -0500

    Create checkboxes for each setting

--- app/containers/Settings.jsx
+++ app/containers/Settings.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, {useCallback } from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+
+import defaultState from '../util/defaultState'
 
 import EditMenu from '../components/editor/EditMenu'
 
-export default function Settings(props) {
-  return <EditMenu tool="settings">
-           <p>settings</p>
-         </EditMenu>
+const OPTIONS = Object.freeze(Object.keys(defaultState.attributes.settings))
+
+function SettingsOption({ option }) {
+  const dispatch = useDispatch()
+  const value = useSelector(state => state.attributes.settings[option])
+
+  const onChange = useCallback(
+    () => dispatch({type: 'UPDATE_SETTING', key: option, value: !value}),
+    [dispatch, value]
+  )
+
+  return <div className="settings-option">
+           <span>{option}: </span>
+           <span>
+             <input type="checkbox" checked={value} onChange={onChange} />
+           </span>
+         </div>
 }
 
-Settings.propTypes = {}
+SettingsOption.propTypes = {
+  option: PropTypes.oneOf(OPTIONS).isRequired
+}
+
+export default function Settings() {
+  return <EditMenu tool="settings">
+           { OPTIONS.map( option => <SettingsOption key={option} option={option} />) }
+         </EditMenu>
+}
