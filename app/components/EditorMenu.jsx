c5157de9450344e6bbd5715aba88081c5039c2ba
Matthew <matthew.skomarovsky@gmail.com>
2020-05-14 13:20:49 -0700

    only map owners can manage editors

--- app/components/EditorMenu.jsx
+++ app/components/EditorMenu.jsx
@@ -1,34 +1,22 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { IconContext } from "react-icons/lib"
 
 import { MENU_ITEMS } from '../editorMenu'
 import EditorMenuItem from './EditorMenuItem'
 import UndoRedo from './UndoRedo'
 import { userIsOwnerSelector } from '../util/selectors'
 
-const iconContextValue = {
-  size: "35px",
-  className: "editor-menu-icon",
-  color: 'gray'
-}
-
 export default function EditorMenu() {
   const isOwner = useSelector(userIsOwnerSelector)
-
-  if (!isOwner) {
-    delete MENU_ITEMS['editors']
-  }
+  const items = isOwner ? MENU_ITEMS : MENU_ITEMS.filter(i => i !== 'editors')
 
   return (
     <div className="editor-menu">
-      <IconContext.Provider value={iconContextValue} >
-        { MENU_ITEMS.map(item => (
-          <EditorMenuItem
-            key={item}
-            item={item} /> 
-        )) }
-      </IconContext.Provider>
+      { items.map(item => (
+        <EditorMenuItem
+          key={item}
+          item={item} />
+      )) }
       <UndoRedo />
     </div>
   )
