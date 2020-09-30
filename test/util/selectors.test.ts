629a6d7662e682786342eab024b572ae6df07a83
ziggy <ziggy@elephant-bird.net>
2020-09-30 15:36:34 -0400

    tests for hasUnsavedChangesSelector

--- test/util/selectors.test.ts
+++ test/util/selectors.test.ts
@@ -2,18 +2,22 @@ import { expect } from 'chai'
 
 import Graph from '../../app/graph/graph'
 import defaultState, { User, StateWithHistory }  from '../../app/util/defaultState'
-import { userIsOwnerSelector } from '../../app/util/selectors'
+import { userIsOwnerSelector, hasUnsavedChangesSelector } from '../../app/util/selectors'
 import merge from 'lodash/merge'
+
+const graphState = { graph: { present: Graph.new(), past: [], future: [] } }
+const defaultUser = { id: 123, name: 'foo', url: 'http://foo.com' }
+
+function mergeState (...objs: Array<any>):  StateWithHistory {
+    return merge({}, defaultState, ...objs)
+}
+
+function stateWithOwner(owner: User | null, user: User | null = defaultUser): StateWithHistory {
+    return mergeState(graphState, { attributes: { user: user, owner: owner } })
+}
 
 describe('Selectors', function() {
     describe('userIsOwnerSelector', function() {
-        const graphState = { graph: { present: Graph.new(), past: [], future: [] } }
-        const defaultUser = { id: 123, name: 'foo', url: 'http://foo.com' }
-
-        function stateWithOwner(owner: User | null, user: User | null = defaultUser): StateWithHistory {
-            return merge({}, defaultState, graphState, { attributes: { user: user, owner: owner } })
-        }
-
         specify('when user and owner are null', function() {
             expect(userIsOwnerSelector(stateWithOwner(null, null))).to.be.false
         })
@@ -30,5 +34,16 @@ describe('Selectors', function() {
         specify('owner is missing', function() {
             expect(userIsOwnerSelector(stateWithOwner(null))).to.be.true
         })
+    })
+
+    describe('hasUnsavedChangesSelector', function() {
+        it('returns false unless in editing mode', function() {
+            let state = mergeState({ display: { modes: { editor: false } } })
+            expect(hasUnsavedChangesSelector(state)).to.be.false
+        })
+
+        it('returns true when graph has changed')
+        it('returns false when viewport has changed')
+        it('returns false when open menus has changed')
     })
 })
