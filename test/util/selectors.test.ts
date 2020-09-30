b7b67588d6c3d4bf22cb0a8233bdd243baa8adb8
ziggy <ziggy@elephant-bird.net>
2020-09-30 16:50:14 -0400

    additional test for hasUnsavedChangesSelector

--- test/util/selectors.test.ts
+++ test/util/selectors.test.ts
@@ -1,13 +1,20 @@
 import { expect } from 'chai'
 import merge from 'lodash/merge'
+import cloneDeep from 'lodash/cloneDeep'
 import Graph from '../../app/graph/graph'
 import Node from '../../app/graph/node'
 import defaultState, { User, StateWithHistory }  from '../../app/util/defaultState'
-import { userIsOwnerSelector, hasUnsavedChangesSelector } from '../../app/util/selectors'
+import { userIsOwnerSelector, hasUnsavedChangesSelector, paramsForSaveSelector } from '../../app/util/selectors'
 
 const graphState = { graph: { present: Graph.new(), past: [], future: [] } }
 const defaultUser = { id: 123, name: 'foo', url: 'http://foo.com' }
 const exampleGraph = Graph.addNode(Graph.new(), { x: 1, y: 1 })
+const exampleGraphWithNewNode = Graph.addNode(cloneDeep(exampleGraph), { x: 2, y: 2 })
+const stateWithGraphInEditorMode = mergeState({ display: { modes: { editor: true } } },
+                                              { graph: { present: exampleGraph, past: [], future: [] } },
+                                              { lastSavedData: null })
+const stateWithLastSavedData = merge({}, stateWithGraphInEditorMode, { lastSavedData:  paramsForSaveSelector(stateWithGraphInEditorMode) })
+const stateWithDifferentLastSavedData = merge({}, stateWithLastSavedData, { graph: { present: exampleGraphWithNewNode, past: [exampleGraph]  } })
 
 function mergeState (...objs: Array<any>):  StateWithHistory {
   return merge({}, defaultState, ...objs)
@@ -44,16 +51,15 @@ describe('Selectors', function() {
     })
 
     it('returns false when there is no lastSavedData', function() {
-      let state = mergeState(
-        { display: { modes: { editor: false } } },
-        { graph: { present: exampleGraph, past: [], future: [] } },
-        { lastSavedData: null }
-      )
-      expect(hasUnsavedChangesSelector(state)).to.be.false
+      expect(hasUnsavedChangesSelector(stateWithGraphInEditorMode)).to.be.false
     })
 
-    it('returns true when graph has changed')
-    it('returns false when viewport has changed')
-    it('returns false when open menus has changed')
+    it('returns false when graph has not changed', function() {
+      expect(hasUnsavedChangesSelector(stateWithLastSavedData)).to.be.false
+    })
+
+    it('returns true when graph has changed', function() {
+      expect(hasUnsavedChangesSelector(stateWithDifferentLastSavedData)).to.be.true
+    })
   })
 })
