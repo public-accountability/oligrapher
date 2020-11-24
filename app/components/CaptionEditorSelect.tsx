4ccca954e7370e826ca8a9712c75b91e1a4ef800
ziggy <ziggy@elephant-bird.net>
2020-11-24 14:40:06 -0500

    upgrade material-ui + fix caption makeStyles typesccript error

--- app/components/CaptionEditorSelect.tsx
+++ app/components/CaptionEditorSelect.tsx
@@ -1,37 +1,37 @@
 import React from 'react'
 import { Select, MenuItem } from '@material-ui/core'
-import { makeStyles } from '@material-ui/styles'
+import { makeStyles } from '@material-ui/core/styles'
 
 import { classNames } from '../util/helpers'
 
-const useStyles = makeStyles(() => ({
-  select: (width: number) => ({
+const useStyles = makeStyles({
+  select: (props: CaptionEditorSelectProps) => ({
     marginTop: 5,
     marginRight: 5,
     fontSize: 12,
     lineHeight: 1.2,
-    width: width
+    width: props.width
   }),
   option: {
     fontSize: 12,
     lineHeight: 1
   }
-}))
+})
 
-export default function CaptionEditorSelect({ value, onChange, options, width, name }: CaptionEditorSelectProps) {
-  const classes = useStyles(width)
-  const classNamez = classNames(classes.select, `select-caption-${name}`)
+export default function CaptionEditorSelect(props: CaptionEditorSelectProps) {
+  const classes = useStyles(props)
+  const classNamez = classNames(classes.select, `select-caption-${props.name}`)
 
   return (
     <Select
-      id={`caption-editor-select-${name}`}
+      id={`caption-editor-select-${props.name}`}
       MenuProps={{ id: 'caption-editor-select-menu', transitionDuration: 0 }}
-      className={classNamez}
-      value={value}
-      onChange={onChange(name)}
+      className={classes.select}
+      value={props.value}
+      onChange={props.onChange(props.name)}
       variant="outlined"
     >
-      { options.map(({ value, label }) => (
+      { props.options.map(({ value, label }) => (
         <MenuItem className={classes.option} dense={true} value={value} key={value}>{label}</MenuItem>)
        ) }
     </Select>
