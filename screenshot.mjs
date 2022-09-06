c04bdf42a932963e7234653ffe54415c9575fc26
ziggy <ziggy@elephant-bird.net>
2022-09-06 18:17:06 -0400

    remove webpack.config

--- screenshot.mjs
+++
@@ -1,8 +0,0 @@
-// import { JSDOM } from 'jsdom'
-// import raf from 'raf'
-// require.extensions['.scss'] = () => null
-// const jsdom = new JSDOM('<!doctype html><html><body></body></html>')
-
-// raf.polyfill(jsdom.window)
-
-// import Oligrapher from './app/Oligrapher.jsx'
