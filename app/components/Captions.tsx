05fd58422c0671474768c85be284367aebfafb0d
ziggy <ziggy@littlesis.org>
2022-10-31 16:58:42 -0400

    fix add caption tool. disable dragging while editing captions.

--- app/components/Captions.tsx
+++ app/components/Captions.tsx
@@ -1,47 +1,47 @@
-import React, { useMemo } from 'react'
-import { useSelector } from '../util/helpers'
-import Caption from './Caption'
-import FloatingEditor from '../util/floatingEditor'
-import { annotationHasHighlightsSelector } from '../util/selectors'
-import { State } from '../util/defaultState'
+import React from "react"
+import { useSelector } from "../util/helpers"
+import Caption from "./Caption"
+import FloatingEditor from "../util/floatingEditor"
+import { annotationHasHighlightsSelector, highlightedCaptionIdsSelector } from "../util/selectors"
+import { State } from "../util/defaultState"
 
-function calculateAppearance(id: string, highlightedIds: string[], annotationHasHighlights: boolean, editMode: boolean): string {
-  if (!annotationHasHighlights) {
+type CaptionAppearance = "normal" | "highlighted" | "faded"
+
+function calculateAppearance(
+  id: string,
+  highlightedIds: string[],
+  annotationHasHighlights: boolean
+): CaptionAppearance {
+  if (annotationHasHighlights) {
+    if (highlightedIds.includes(id)) {
+      return "highlighted"
+    } else {
+      return "faded"
+    }
+  } else {
     return "normal"
   }
-
-  if (highlightedIds.includes(id)) {
-    return "highlighted"
-  }
-
-  if (editMode) {
-    return "normal"
-  }
-
-  return "faded"
 }
 
 export default function Captions() {
   const captions = useSelector<State>(state => state.graph.captions)
-  const editedCaptionId = useSelector<State>(state => FloatingEditor.getId(state.display, 'caption'))
-  const storyMode = useSelector<State>(state => state.display.modes.story)
-  const { list, currentIndex } = useSelector<State>(state => state.annotations)
-  const highlightedCaptionIds = useMemo(
-    () => storyMode ? (list[currentIndex]?.captionIds || []) : [],
-    [storyMode, list, currentIndex]
+  const editedCaptionId = useSelector<State>(state =>
+    FloatingEditor.getId(state.display, "caption")
   )
+  const highlightedCaptionIds = useSelector(highlightedCaptionIdsSelector)
   const annotationHasHighlights = useSelector(annotationHasHighlightsSelector)
   const editMode = useSelector<State>(state => state.display.modes.editor)
 
   return (
     <g className="captions">
-      { Object.entries(captions).map(([id, caption]) => (
+      {Object.entries(captions).map(([id, caption]) => (
         <Caption
           key={id}
           caption={caption}
           currentlyEdited={editMode && id === editedCaptionId}
-          status={calculateAppearance(id, highlightedCaptionIds, annotationHasHighlights, editMode)} />
-      )) }
+          status={calculateAppearance(id, highlightedCaptionIds, annotationHasHighlights)}
+        />
+      ))}
     </g>
   )
 }
