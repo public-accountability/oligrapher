c911596bbff6a165c47a21e79aa3c014be8aa45a
Joseph Lacey <joseph@offkeydigital.com>
2026-03-20 11:24:33 -0700

    Migrating ClassicEditor to new syntaxi, part 2

--- app/components/AnnotationTextEditor.tsx
+++ app/components/AnnotationTextEditor.tsx
@@ -1,6 +1,97 @@
 import React, { useCallback } from "react"
 import { CKEditor } from "@ckeditor/ckeditor5-react"
-import { ClassicEditor } from "@ckeditor/ckeditor5"
+import {
+    ClassicEditor,
+    Essentials,
+    CKFinderUploadAdapter,
+    Autoformat,
+    Bold,
+    Italic,
+    BlockQuote,
+    CKBox,
+    CKFinder,
+    EasyImage,
+    Heading,
+    Image,
+    ImageCaption,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload,
+    PictureEditing,
+    Indent,
+    Link,
+    List,
+    MediaEmbed,
+    Paragraph,
+    PasteFromOffice,
+    Table,
+    TableToolbar,
+    TextTransformation,
+    CloudServices
+} from 'ckeditor5';
+
+import 'ckeditor5/ckeditor5.css';
+
+class Editor extends ClassicEditor {
+    static builtinPlugins = [
+        Essentials,
+        CKFinderUploadAdapter,
+        Autoformat,
+        Bold,
+        Italic,
+        BlockQuote,
+        CKBox,
+        CKFinder,
+        CloudServices,
+        EasyImage,
+        Heading,
+        Image,
+        ImageCaption,
+        ImageStyle,
+        ImageToolbar,
+        ImageUpload,
+        Indent,
+        Link,
+        List,
+        MediaEmbed,
+        Paragraph,
+        PasteFromOffice,
+        PictureEditing,
+        Table,
+        TableToolbar,
+        TextTransformation
+    ];
+
+    static defaultConfig = {
+        toolbar: {
+            items: [
+                'undo', 'redo',
+                '|', 'heading',
+                '|', 'bold', 'italic',
+                '|', 'link', 'uploadImage', 'insertTable', 'blockQuote', 'mediaEmbed',
+                '|', 'bulletedList', 'numberedList', 'outdent', 'indent'
+            ]
+        },
+        image: {
+            toolbar: [
+                'imageStyle:inline',
+                'imageStyle:block',
+                'imageStyle:side',
+                '|',
+                'toggleImageCaption',
+                'imageTextAlternative'
+            ]
+        },
+        table: {
+            contentToolbar: [
+                'tableColumn',
+                'tableRow',
+                'mergeTableCells'
+            ]
+        },
+        language: 'en'
+    };
+}
 
 const nullifyIfEmpty = (s: string): string | null => (s === "" ? null : s)
 
@@ -33,7 +124,7 @@ export default function AnnotationTextEditor({ text, onChange }: AnnotationTextE
 
   return (
     <CKEditor
-      editor={ClassicEditor}
+      editor={Editor}
       data={text || "<p></p>"}
       onChange={handleChange}
       config={config}
