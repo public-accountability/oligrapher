6567bf0f91439984796d278b3c059dd48a701b0c
Joseph Lacey <joseph@offkeydigital.com>
2026-03-20 11:19:22 -0700

    Migrating ClassicEditor to new syntax

--- app/components/AnnotationTextEditor.tsx
+++ app/components/AnnotationTextEditor.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from "react"
 import { CKEditor } from "@ckeditor/ckeditor5-react"
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
+import { ClassicEditor } from "@ckeditor/ckeditor5"
 
 const nullifyIfEmpty = (s: string): string | null => (s === "" ? null : s)
 
