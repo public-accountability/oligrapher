c8f8279c5dddb0b1c7c5fc9c21cca56de0b482f3
Joseph Lacey <joseph@offkeydigital.com>
2026-03-20 12:49:54 -0700

    Migrating ClassicEditor to new syntax, part 4

--- app/components/AnnotationTextEditor.tsx
+++ app/components/AnnotationTextEditor.tsx
@@ -32,6 +32,68 @@ import {
 
 import 'ckeditor5/ckeditor5.css';
 
+class Editor extends ClassicEditor {
+    static builtinPlugins = [
+        Essentials,
+        CKFinderUploadAdapter,
+        Autoformat,
+        Bold,
+        Italic,
+        BlockQuote,
+        CKBox,
+        CKFinder,
+        CloudServices,
+        EasyImage,
+        Heading,
+        Image,
+        ImageCaption,
+        ImageStyle,
+        ImageToolbar,
+        ImageUpload,
+        Indent,
+        Link,
+        List,
+        MediaEmbed,
+        Paragraph,
+        PasteFromOffice,
+        PictureEditing,
+        Table,
+        TableToolbar,
+        TextTransformation
+    ];
+
+    static defaultConfig = {
+        licenseKey: 'GPL',
+        toolbar: {
+            items: [
+                'undo', 'redo',
+                '|', 'heading',
+                '|', 'bold', 'italic',
+                '|', 'link', 'uploadImage', 'insertTable', 'blockQuote', 'mediaEmbed',
+                '|', 'bulletedList', 'numberedList', 'outdent', 'indent'
+            ]
+        },
+        image: {
+            toolbar: [
+                'imageStyle:inline',
+                'imageStyle:block',
+                'imageStyle:side',
+                '|',
+                'toggleImageCaption',
+                'imageTextAlternative'
+            ]
+        },
+        table: {
+            contentToolbar: [
+                'tableColumn',
+                'tableRow',
+                'mergeTableCells'
+            ]
+        },
+        language: 'en'
+    };
+}
+
 const nullifyIfEmpty = (s: string): string | null => (s === "" ? null : s)
 
 type AnnotationTextEditorPropTypes = {
